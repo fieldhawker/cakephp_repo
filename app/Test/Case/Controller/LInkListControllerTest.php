1eef31c98cf666d101268307764cfc8ccd5783c5
PC839\bp-takano <fieldhawker@gmail.com>
2017-12-25 15:17:16 +0900

    インデントの修正

--- app/Test/Case/Controller/LInkListControllerTest.php
+++ app/Test/Case/Controller/LInkListControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,8 +9,10 @@
 require_once CAKE_CORE_INCLUDE_PATH . DS . 'CAKE' . DS . 'TestSuite' . DS . 'ControllerTestCase.php';
 
 class LinkListControllerTest extends ControllerTestCase {
-  public function testIndex() {
-    $this->testAction('/linklist/');
-    $this->assertTextContains('LinkList/index.ctp', $this->view);
-  }
+
+    public function testIndex() {
+        $this->testAction('/linklist/');
+        $this->assertTextContains('LinkList/index.ctp', $this->view);
+    }
+
 }
